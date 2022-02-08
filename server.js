2ef76ccc765fb2d460061933c3f3382313644557
heavypete <70510263+heavypete@users.noreply.github.com>
2022-02-08 14:34:33 +0100

    express serving running

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-let server;
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello, I am the PRP backend!");
+});
+
+app.listen(port, function () {
+  console.log(`PRP Backend App listening on port ${port}!`);
+});
