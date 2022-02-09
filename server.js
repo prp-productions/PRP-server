46aedaae5dd878cd1c9dfe2a709da5f35189afb2
heavypete <70510263+heavypete@users.noreply.github.com>
2022-02-09 13:31:08 +0100

    add nodemon, resources.txt, app.get.test route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   res.send("Hello, I am the PRP backend!");
 });
 
+app.get("/test-route", function (req, res) {
+  res.send("This is a PRP test route");
+});
+
 app.listen(port, function () {
   console.log(`PRP Backend App listening on port ${port}!`);
 });
