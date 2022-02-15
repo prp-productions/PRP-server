3fda3e7da0eb339450b031473643e2e84b18eec9
heavypete <70510263+heavypete@users.noreply.github.com>
2022-02-15 10:23:51 +0100

    cookieparser, express, sessions, nodemon

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
 
+//* middleware
+app.use(cookieParser());
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "secret",
+  })
+);
+
+//* basic endpoints
 app.get("/", function (req, res) {
-  res.send("Hello, I am the PRP backend!");
+  res.send("Hello, I am the PRP backend (endpoint test)!");
 });
 
-app.get("/test-route", function (req, res) {
+app.get("/test", function (req, res) {
   res.send("This is a PRP test route");
 });
 
